6ece330b7ce6db3602aa0e90454cb99f7389590d
Cristian Puhl <cristian.puhl@universo.univates.br>
2025-10-22 11:38:15 -0300

    Project configuration

---
+++ src/main/java/br/com/saga_orquestrado/product_validation/ProductValidationApplication.java
@@ -0,0 +1,13 @@
+package br.com.saga_orquestrado.product_validation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductValidationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductValidationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/saga_orquestrado/product_validation/ProductValidationApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.saga_orquestrado.product_validation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductValidationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
