4a66c06f67f2651e5ae51794444ebf3c94a22b82
Cristian Puhl <cristian.puhl@universo.univates.br>
2025-10-22 21:49:22 -0300

    Structuring classes and DTOs

---
+++ src/main/java/br/com/saga_orquestrado/product_validation/core/dto/Order.java
@@ -0,0 +1,23 @@
+package br.com.saga_orquestrado.product_validation.core.dto;
+
+import br.com.saga_orquestrado.order.core.document.OrderProducts;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Order {
+    private String id;
+    private List<OrderProducts> products;
+    private LocalDateTime createdAt;
+    private String transactionId;
+    private double totalAmount;
+    private int totalItems;
+}
